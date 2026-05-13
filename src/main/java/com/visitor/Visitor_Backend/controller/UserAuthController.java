bbf4e26b530265250548449644ad7331fc43a963
ruchitajadhav27 <jadhavruchita27@gmail.com>
2026-05-13 11:55:42 +0530

    chnages  deploy

--- src/main/java/com/visitor/Visitor_Backend/controller/UserAuthController.java
+++ src/main/java/com/visitor/Visitor_Backend/controller/UserAuthController.java
@@ -52,8 +52,9 @@ public class UserAuthController {
 
             System.out.println("========= OTP API HIT =========");
             
-            System.out.println("MAIL HOST = " + System.getProperty("spring.mail.host"));
-            System.out.println("MAIL PORT = " + System.getProperty("spring.mail.port"));
+            System.out.println("MAIL HOST = " + mailHost);
+            System.out.println("MAIL PORT = " + mailPort);
+            System.out.println("SENDER = " + senderEmail);
             System.out.println("SENDER = " + senderEmail);
             
             System.out.println("MAIL USER = " + senderEmail);
